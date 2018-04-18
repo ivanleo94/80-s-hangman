6ff9897bd4dee05e3cba3078828d833c33dd4b23
ivanleo94 <31164505+ivanleo94@users.noreply.github.com>
2018-04-17 20:03:20 -0700

    Update game.js

--- javascript/game.js
+++ javascript/game.js
@@ -1,69 +1,197 @@
-var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var gameObject = {
+	currentLetter: "",
 
-//Setting all to zero
-var wins = 0;
-var losses = 0;
-var guesses = 9;
-var guessesLeft = 9;
-var guessedLetters = [];
-var letterToGuess = null;
+	allGuesses: [],
+	incorrectGuesses: [],
+	correctGuesses: [],
+	correctGuessesInOrder: [],
 
+	bandsArray: ["Wu tang", "ATCQ", "Mobb Deep", "Digable Planets", "Beastie Boys", "Run DMC", "NWA", "Outkast", "LONS"],
+	randomWord: "",
+	bandLetters:[],
 
+	isMatch: null,
+	isRepeat: null,
 
-//Lets the computer select a random letter from the available choices
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+	guessesRemaining: 15,
+	loseCount: 0,
+	winCount:0,
 
-//Allows the user 9 guesses
-// guesses = guesses || 9
-var updateGuessesLeft = function() {
-  // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
-  document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
-};
+	generateWord: function(){
+		//Generate a random number from 0-8
+		var random_num = Math.random() * 9;
+		random_num = Math.floor(random_num);
 
-var updateLetterToGuess = function() {
-  this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
-};
-var updateGuessesSoFar = function() {
-  // Here we take the guesses the user has tried -- then display it as letters separated by commas. 
-  document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
-};
-// Function will be called when we reset everything
-var reset = function() {
-  totalGuesses = 9;
-  guessesLeft = 9;
-  guessedLetters = [];
+		//Assign randomWord to a word from the array whose index was chosen randomly.
+		//Split the string into an array containing the individual letters of the randomly chosen word
+		this.randomWord = this.bandsArray[random_num];
+		this.bandLetters = this.randomWord.split("");
 
-  updateLetterToGuess();
-  updateGuessesLeft();
-  updateGuessesSoFar();
+		//Shows that a randomly chosen band name was converted into an array containing each of its letters.
+		console.log(this.randomWord + " " + this.bandLetters);
+
+		//Since this function will only run on a win/loss, reset the guesses arrays
+		this.allGuesses = [];
+		this.incorrectGuesses = [];
+		this.correctGuesses = [];
+		this.correctGuessesInOrder = [];
+		this.guessesRemaining = 15;
+	},
+
+	checkRepeat: function(){
+		var repeatCounter = -1;
+
+		//Loop for the number of guesses previously made amount of times.
+		//If the current letter equals one from the array of allGuesses, the counter variable counts up one.
+		for (var i=0; i < this.allGuesses.length; i++){
+			if (this.currentLetter == this.allGuesses[i]){
+				repeatCounter++;
+			}
+		}
+		//If counter is zero, the global isRepeat variable becomes false (signifying no matches found)
+		//Otherwise a match was found and isRepeat becomes true.
+		if (repeatCounter == 0){
+			this.isRepeat = false;
+		}
+		else{
+			this.isRepeat = true;
+		}
+	},
+	checkMatch: function(){
+		var matchCounter = 0;
+
+		//Loop for the band names length amount of times.
+		//If the guessed letter is equal to the the bands letter at a given index, the counter variable counts up one.
+		for (var i=0; i < this.bandLetters.length; i++){
+			if (this.currentLetter == this.bandLetters[i]){
+				matchCounter++;
+			}
+		}
+		//If counter is zero, the global isMatch variable becomes false (signifying no matches found)
+		//Otherwise a match was found and isMatch becomes true.
+		if (matchCounter == 0){
+			this.isMatch = false;
+		}
+		else{
+			this.isMatch = true;
+		}
+	},
+	match_repeatComparison: function(){
+		//If the same key is pressed twice, it is removed from allGuesses.
+		if (this.isRepeat == true){
+			this.allGuesses.pop(this.currentLetter);
+		}
+		//Letter has not been guessed and was a wrong guess, put the currentLetter in incorrectGuesses.
+		if (this.isRepeat == false && this.isMatch == false){
+			this.incorrectGuesses.push(this.currentLetter);
+			this.guessesRemaining--;
+		}
+		//Letter has not been guessed and was a correct guess, put the currentLetter in correctGuesses.
+		if (this.isRepeat == false && this.isMatch == true){
+			this.correctGuesses.push(this.currentLetter);
+			this.guessesRemaining--;
+		}
+	},
+	revealBand: function(){
+		//If there are no correctGuesses,
+		//For the number of letters in the bands name, fill the displayed guesses with an underscore.
+		if (this.correctGuesses.length == 0){
+			for (var i =0; i<this.bandLetters.length; i++){
+				this.correctGuessesInOrder[i] = "_";
+			}
+		}
+		else {
+			//For the length of the bands name,
+			for (var i=0; i<this.bandLetters.length; i++){
+				//If the displayed guess is not the same as bandletters at index i,
+				if (this.correctGuessesInOrder[i] != this.bandLetters[i]){
+					//Loop for correctGuesses length number of times,
+					for (var j=0; j<this.correctGuesses.length; j++){
+						//If the correctGuesses at j is equal to bandLetters at i, the displayedGuess becomes the bandletter at index i
+						if (this.correctGuesses[j] == this.bandLetters[i]){
+							this.correctGuessesInOrder[i] = this.bandLetters[i];
+						}
+						//Otherwise the displayedGuess at index i (corresponding to the band letter's indexes) becomes an underscore.
+						else {
+							this.correctGuessesInOrder[i] = "_";
+						}
+					}
+				}
+			}
+		}
+
+		document.getElementById("current-word").innerHTML = this.correctGuessesInOrder.join(" ");
+		document.getElementById("num-wins").innerHTML = ("Wins: " + this.winCount + "  " + "Losses: " + this.loseCount);
+		document.getElementById("letters-guessed").innerHTML = this.incorrectGuesses;
+		document.getElementById("guesses-remaining").innerHTML = this.guessesRemaining;
+	},
+	checkProgress: function(){
+		var counter = 0;
+
+		//Loop a number of times equal to the length of the band name. 
+		//If a guess is equal to the the band letter at the same index, add 1 to the counter.
+		for (var i=0; i<this.bandLetters.length; i++){
+			if (this.correctGuessesInOrder[i] == this.bandLetters[i]){
+				counter++;
+			}
+		}
+
+		//If the counter is the length of the band name, the user has won.
+		if (counter == this.bandLetters.length){
+			alert("You win");
+			this.winCount++;
+			this.generateWord();
+		}
+		//If the number of guesses remaining is zero, the user has lost.
+		if (this.guessesRemaining == 0){
+			alert("You lose!");
+			this.loseCount++;
+			this.generateWord();
+		}
+	}
 }
 
-updateLetterToGuess();
-updateGuessesLeft();
+var userStartedGameOnce = false;
+
+//On every keyup...
+document.onkeyup = function(q) {
+
+	//currentLetter is grabbed from the keyboard and converted to upper case.
+	//Then the letter is pushed into the allGuesses array
+	gameObject.currentLetter = String.fromCharCode(q.keyCode).toUpperCase();
+
+	//If the user presses the space button upon loading the page, start the game.
+	if (gameObject.currentLetter == " " && userStartedGameOnce == false){
 
 
-//When key is released it becomes the users guess
-document.onkeyup = function(event) {
-    guessesLeft--;
-  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+		gameObject.generateWord();
 
-  guessedLetters.push(userGuess);
-  updateGuessesLeft();
-  updateGuessesSoFar();
+		userStartedGameOnce = true;
 
-        if (guessesLeft > 0){
-            if (userGuess == letterToGuess){
-                wins++;
-                document.querySelector('#wins').innerHTML = "Wins: " + wins;
-                alert("Yes, you are psychic!");
-                reset();
-            }
-        }else if(guessesLeft == 0){
-            // Then we will loss and we'll update the html to display the loss 
-            losses++;
-            document.querySelector('#losses').innerHTML = "Losses: " + losses;
-            alert("Sorry, you're not psychic, maybe try again?");
-            // Then we'll call the reset. 
-            reset();
-        }
-};
+	}
+
+	gameObject.allGuesses.push(gameObject.currentLetter);
+
+	console.log("Current Letter: " + gameObject.currentLetter + "\n" + "Band Letters: " + gameObject.bandLetters + "\n" + "All Guesses: " + gameObject.allGuesses);
+
+
+	//Checks to see if the letter has been typed before.
+	//Checks to see if the letter matches with one in the band name.
+	gameObject.checkRepeat();
+	gameObject.checkMatch();
+
+
+	//This function determines which array to push the currentLetter into.
+	gameObject.match_repeatComparison();
+
+	console.log("Correct Guesses: " + gameObject.correctGuesses);
+	console.log("Incorrect Guesses: " + gameObject.incorrectGuesses);
+	console.log("Guesses Remaining:" + gameObject.guessesRemaining);
+
+	//Reveals the band name as it is being guessed.
+	gameObject.revealBand();
+	console.log(gameObject.correctGuessesInOrder);
+
+	//Check to see if the game is still in progress or if a win/lose has happened
+	gameObject.checkProgress();
+}
